2f274e88de0ad49abd13602480ff583d930dbcf8
Davide Carroccio <146735260+davcarro@users.noreply.github.com>
2023-12-06 11:38:43 +0100

    Update hello.py

--- auto-scaling-web-app/hello.py
+++ auto-scaling-web-app/hello.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/app')
 def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=2000)
